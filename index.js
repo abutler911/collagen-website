7ceacde29c4bf079e97612d562c56e8f6f0b8d6b
abutler911 <abutler911@gmail.com>
2023-02-28 18:26:58 -0700

    added new image gallery

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-$(".navTrigger").click(function () {
-  $(this).toggleClass("active");
-  console.log("Clicked menu");
-  $("#mainListDiv").toggleClass("show_list");
-  $("#mainListDiv").fadeIn();
+const imageArray = [
+  "./public/imgs/testimonials/testimonial1.jpg",
+  "./public/imgs/testimonials/testimonial2.jpg",
+  "./public/imgs/testimonials/testimonial3.jpg",
+  "./public/imgs/testimonials/testimonial4.jpg",
+  "./public/imgs/testimonials/testimonial5.jpg",
+  "./public/imgs/testimonials/testimonial6.jpg",
+  "./public/imgs/testimonials/testimonial7.png",
+  "./public/imgs/testimonials/testimonial8.png",
+  "./public/imgs/testimonials/testimonial9.png",
+  "./public/imgs/testimonials/testimonial10.png",
+  "./public/imgs/testimonials/testimonial11.jpg",
+  "./public/imgs/testimonials/testimonial12.jpg",
+  "./public/imgs/testimonials/testimonial13.jpg",
+];
+
+const randomImage = document.querySelector("#random-image-button");
+const imageGallery = document.querySelector(".image-gallery");
+
+const getRandomImage = function () {
+  const randomNumber = Math.floor(Math.random() * imageArray.length);
+  const image = imageArray[randomNumber];
+  return image;
+};
+
+randomImage.addEventListener("click", () => {
+  const image = getRandomImage();
+  const imageElement = document.createElement("img");
+  const imageContainer = document.createElement("div");
+
+  imageElement.setAttribute("src", image);
+  imageElement.classList.add("img-fluid", "img-thumbnail");
+
+  imageContainer.classList.add("col-md-4", "mt-3", "col-lg-3");
+  imageContainer.appendChild(imageElement);
+
+  imageGallery.innerHTML = "";
+  imageGallery.appendChild(imageContainer);
 });
