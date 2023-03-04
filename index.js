ebea633fd746cd74dec69693c5114b7eca447040
abutler911 <abutler911@gmail.com>
2023-03-04 08:51:55 -0700

    added random quotes under main product

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const quoteContainer = document.querySelector("#quote");
+const author = document.querySelector("#author");
+// const quoteButton = document.querySelector("#quote-btn");
+let url = "https://type.fit/api/quotes";
+
+// quoteButton.addEventListener("click", () => {
+//   getQuote().then((quote) => {
+//     quoteContainer.innerHTML = quote.text;
+//     if (!author) {
+//       author.innerHTML = `-- Anonymous`;
+//     }
+//     author.innerHTML = `-- ${quote.author}`;
+//   });
+// });
+
+const getQuote = async function () {
+  const randomNumber = Math.floor(Math.random() * 1000);
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data[randomNumber];
+};
+
+getQuote().then((quote) => {
+  quoteContainer.innerHTML = quote.text;
+  if (!author) {
+    author.innerHTML = `-- Anonymous`;
+  }
+  author.innerHTML = `- ${quote.author}`;
+});
+
 // const imageArray = [
 //   "./public/imgs/testimonials/testimonial1.jpg",
 //   "./public/imgs/testimonials/testimonial2.jpg",
