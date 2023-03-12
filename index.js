fb693adef9e7855b5682416c49230405b78714c3
abutler911 <abutler911@gmail.com>
2023-03-12 14:30:46 -0600

    dynamically insert images into photo-gallery with js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const quoteContainer = document.querySelector("#quote");
 const author = document.querySelector("#author");
+const photoGallery = document.querySelector("#photo-gallery");
 
-// const quoteButton = document.querySelector("#quote-btn");
 let url = "https://type.fit/api/quotes";
-
-// quoteButton.addEventListener("click", () => {
-//   getQuote().then((quote) => {
-//     quoteContainer.innerHTML = quote.text;
-//     if (!author) {
-//       author.innerHTML = `-- Anonymous`;
-//     }
-//     author.innerHTML = `-- ${quote.author}`;
-//   });
-// });
 
 const getQuote = async function () {
   const randomNumber = Math.floor(Math.random() * 1000);
@@ -32,100 +22,38 @@ getQuote().then((quote) => {
 
 AOS.init();
 
-// const imageArray = [
-//   "./public/imgs/testimonials/testimonial1.jpg",
-//   "./public/imgs/testimonials/testimonial2.jpg",
-//   "./public/imgs/testimonials/testimonial3.jpg",
-//   "./public/imgs/testimonials/testimonial4.jpg",
-//   "./public/imgs/testimonials/testimonial5.jpg",
-//   "./public/imgs/testimonials/testimonial6.jpg",
-//   "./public/imgs/testimonials/testimonial7.png",
-//   "./public/imgs/testimonials/testimonial8.png",
-//   "./public/imgs/testimonials/testimonial9.png",
-//   "./public/imgs/testimonials/testimonial10.png",
-//   "./public/imgs/testimonials/testimonial11.jpg",
-//   "./public/imgs/testimonials/testimonial12.jpg",
-//   "./public/imgs/testimonials/testimonial13.jpg",
-//   "./public/imgs/testimonials/testimonial14.jpg",
-//   "./public/imgs/testimonials/testimonial15.png",
-//   "./public/imgs/testimonials/testimonial16.jpg",
-//   "./public/imgs/testimonials/testimonial17.png",
-//   "./public/imgs/testimonials/testimonial18.png",
-//   "./public/imgs/testimonials/testimonial19.jpg",
-//   "./public/imgs/testimonials/testimonial20.jpg",
-// ];
+const photos = [
+  "./public/imgs/testimonials/testimonial1.jpg",
+  "./public/imgs/testimonials/testimonial2.jpg",
+  "./public/imgs/testimonials/testimonial3.jpg",
+  "./public/imgs/testimonials/testimonial4.jpg",
+  "./public/imgs/testimonials/testimonial5.jpg",
+  "./public/imgs/testimonials/testimonial6.jpg",
+  "./public/imgs/testimonials/testimonial7.png",
+  "./public/imgs/testimonials/testimonial8.png",
+  "./public/imgs/testimonials/testimonial9.png",
+  "./public/imgs/testimonials/testimonial10.png",
+  "./public/imgs/testimonials/testimonial11.jpg",
+  "./public/imgs/testimonials/testimonial12.jpg",
+  "./public/imgs/testimonials/testimonial13.jpg",
+  "./public/imgs/testimonials/testimonial14.jpg",
+  "./public/imgs/testimonials/testimonial15.png",
+  "./public/imgs/testimonials/testimonial16.jpg",
+  "./public/imgs/testimonials/testimonial17.png",
+  "./public/imgs/testimonials/testimonial18.png",
+  "./public/imgs/testimonials/testimonial19.jpg",
+  "./public/imgs/testimonials/testimonial20.jpg",
+];
 
-// const getRandomImage = function () {
-//   const randomNumber = Math.floor(Math.random() * imageArray.length); //generate a random number between 1 and the image array length
-//   const image = imageArray[randomNumber]; // assign image the imagearray element at position random number
-//   imageArray.splice(randomNumber, 1); //remove the element from the array so it won't be displayed again
-//   return image;
-// };
+for (i = 0; i < photos.length; i++) {
+  const element = document.createElement("div");
+  element.setAttribute("class", "col-md-4 mt-3 col-lg-3");
 
-// const randomImage = document.querySelector("#random-image-button");
-// const imageGallery = document.querySelector(".image-gallery");
-
-// randomImage.addEventListener("click", () => {
-//   if (imageArray.length === 1) {
-//     // check if the image array is empty - if it is, repopulate it
-//     imageArray.push(
-//       "./public/imgs/testimonials/testimonial1.jpg",
-//       "./public/imgs/testimonials/testimonial2.jpg",
-//       "./public/imgs/testimonials/testimonial3.jpg",
-//       "./public/imgs/testimonials/testimonial4.jpg",
-//       "./public/imgs/testimonials/testimonial5.jpg",
-//       "./public/imgs/testimonials/testimonial6.jpg",
-//       "./public/imgs/testimonials/testimonial7.png",
-//       "./public/imgs/testimonials/testimonial8.png",
-//       "./public/imgs/testimonials/testimonial9.png",
-//       "./public/imgs/testimonials/testimonial10.png",
-//       "./public/imgs/testimonials/testimonial11.jpg",
-//       "./public/imgs/testimonials/testimonial12.jpg",
-//       "./public/imgs/testimonials/testimonial13.jpg",
-//       "./public/imgs/testimonials/testimonial14.jpg",
-//       "./public/imgs/testimonials/testimonial15.png",
-//       "./public/imgs/testimonials/testimonial16.jpg",
-//       "./public/imgs/testimonials/testimonial17.png",
-//       "./public/imgs/testimonials/testimonial18.png",
-//       "./public/imgs/testimonials/testimonial19.jpg",
-//       "./public/imgs/testimonials/testimonial20.jpg"
-//     );
-//   }
-//   const image = getRandomImage();
-
-//   const imageElement = document.createElement("img");
-//   imageElement.setAttribute("src", image);
-//   imageElement.classList.add("img-fluid", "img-thumbnail");
-
-//   const imageContainer = document.createElement("div");
-//   imageContainer.classList.add("col-md-12", "mt-3", "col-lg-12");
-//   imageContainer.appendChild(imageElement);
-
-//   imageGallery.innerHTML = "";
-//   imageGallery.appendChild(imageContainer);
-// });
-
-// window.addEventListener("scroll", function () {
-//   var navbar = document.getElementById("navbar");
-//   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-//   if (scrollTop > 0) {
-//     navbar.classList.add("sticky");
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// });
-
-// window.onscroll = function () {
-//   myFunction();
-// };
-
-// function myFunction() {
-//   if (
-//     document.body.scrollTop > 100 ||
-//     document.documentElement.scrollTop > 100
-//   ) {
-//     const element = document.querySelector("#collagen-bottle-image");
-//     element.classList.add("slideUp");
-//   }
-// }
+  const image = document.createElement("img");
+  image.setAttribute("class", "img-fluid img-thumbnail");
+  image.setAttribute("data-aos", "fade");
+  image.setAttribute("data-aos-duration", "2000");
+  image.setAttribute("src", photos[i]);
+  element.appendChild(image);
+  photoGallery.appendChild(element);
+}
